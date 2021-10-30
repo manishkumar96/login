6483ec1ab68090bcef49afa38462e3b941576269
Manish Kumar <manish96.apptunix@gmail.com>
2021-10-30 13:25:52 +0530

    latest commit on 30 Oct

--- lib/helpers/api_constants.dart
+++ lib/helpers/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = "https://waiveapp.herokuapp.com";
+  static const String baseUrl = "your base url";
   static const String register = baseUrl + "/api/user";
   static const String createOtp = baseUrl + "/api/otp/create";
   static const String verifyOtp = baseUrl + "/api/otp/verify";
